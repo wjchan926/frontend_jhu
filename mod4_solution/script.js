bfef6f6645dc4e5076e3bf8bee93927184b46ac8
Wesley Chan <wesley.j.chan@gmail.com>
2023-02-17 00:51:08 -0500

    Update Assignment content

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -29,37 +29,71 @@ WARNING!!! WARNING!!!
 
 */
 
-// STEP 1:
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
-(function (){
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function () {
+  const names = [
+    "Yaakov",
+    "John",
+    "Jen",
+    "Jason",
+    "Paul",
+    "Frank",
+    "Larry",
+    "Paula",
+    "Laura",
+    "Jim",
+  ];
 
-// STEP 10:
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
-// See Lecture 50, part 1
-for (let i = 0; i < names.length; i++) {
-
-  // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
-
-  // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  } else {
-    // helloSpeaker.xxxx
+  ////////
+  // #1 //
+  ////////
+  for (let i = 0; i < names.length; i++) {
+    const firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === "j") {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
   }
-}
+
+  ////////
+  // #2 //
+  ////////
+  /**
+   * addGreeting adds the corresponding greeting based on the supplied name.
+   * @param {string} name
+   * @returns Greeting concatenated to name
+   */
+  function addGreeting(name) {
+    const firstLetter = name.charAt(0).toLowerCase();
+    if (firstLetter === "j") {
+      return byeSpeaker.speakSimple(name);
+    }
+    return helloSpeaker.speakSimple(name);
+  }
+
+  // Create array with map
+  const greetingNameArr = names.map(addGreeting);
+  
+  // Log out each greeting and name in array to browser console
+  greetingNameArr.forEach((greetingName) => console.log(greetingName));
+
+  ////////
+  // #3 //
+  ////////
+  const { hello, bye } = names.reduce(
+    (separatedNamesObj, name) => {
+      const firstLetter = name.charAt(0).toLowerCase();
+      if (firstLetter === "j") {
+        separatedNamesObj.bye.push(byeSpeaker.speakSimple(name));
+        return separatedNamesObj;
+      }
+      separatedNamesObj.hello.push(helloSpeaker.speakSimple(name));
+      return separatedNamesObj;
+    },
+    { hello: [], bye: [] }
+  );
+
+  // Log out seperated greeting and names to console
+  hello.forEach(greetingName => console.log(greetingName));
+  bye.forEach(greetingName => console.log(greetingName));
 })();
